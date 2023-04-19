858395044f12ef616cff153788c5cb170d5e770e
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-04-20 01:49:19 +0300

    start

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  Button,
+} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const nameHandler = text => setName(text);
+  const passwordHandler = text => setPassword(text);
+
+  const onLogin = () => {
+    Alert.alert('Credentials', `${name} + ${password}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+        >
+          <TextInput
+            value={name}
+            onChangeText={nameHandler}
+            placeholder="Username"
+            style={styles.input}
+          />
+          <TextInput
+            value={password}
+            onChangeText={passwordHandler}
+            placeholder="Password"
+            secureTextEntry={true}
+            style={styles.input}
+          />
+          <Button title={'Login'} style={styles.input} onPress={onLogin} />
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
   },
 });
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>React Native It`s so hard</Text>
+//       <TouchableOpacity style={styles.btn}>
+//         <Text>Login</Text>
+//       </TouchableOpacity>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#000',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     color: 'white',
+//   },
+
+//   btn: {
+//     backgroundColor: 'white',
+//     borderRadius: 10,
+//     height: 50,
+//     width: 150,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     marginTop: 130,
+//   },
+// });
