4dde3685a4eae8b162551c28eae6633485705771
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-06-08 02:16:03 +0300

    map screen ref

--- screens/MainScreens/MapScreen.js
+++ screens/MainScreens/MapScreen.js
@@ -1,11 +1,26 @@
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { View } from 'react-native';
 
-export const MapScreen = () => {
-  //   const { latitude, longitude } = route.params.location;
+export const MapScreen = ({ route }) => {
+  const { latitude, longitude } = route.params.location;
   return (
     <View style={{ flex: 1 }}>
-      <MapView style={{ flex: 1 }}></MapView>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude,
+          longitude,
+          latitudeDelta: 0.001,
+          longitudeDelta: 0.006,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude,
+            longitude,
+          }}
+        />
+      </MapView>
     </View>
   );
 };
